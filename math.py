28225a51dd1de2228169d72ec9a18ee2100ed229
aditya_mishra <aditya_mishra@persistent.com>
2022-03-20 01:08:59 +0530

    Implemented multiply() fn from local repo

--- math.py
+++ math.py
@@ -6,7 +6,7 @@ def subtract(x,y):
 	return x-y         # from remote repository
 # Multiply implementation
 def multiply(x,y):
-	pass
+	return x*y             # from local repo
 # Divide implementation
 def divide(x,y):
 	pass
